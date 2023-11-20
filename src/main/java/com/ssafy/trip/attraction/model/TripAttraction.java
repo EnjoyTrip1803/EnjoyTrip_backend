b54fd145bce3f37c961ffb80c536f6b0b982b9f6
서성원 <zsa332@gmail.com>
2023-11-20 17:17:27 +0900

    Feat: 여행 세부 계획 dto 추가

---
+++ src/main/java/com/ssafy/trip/attraction/model/TripAttraction.java
@@ -0,0 +1,16 @@
+package com.ssafy.trip.attraction.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@ApiModel(value = "TripAttraction (여행 계획 세부 정보)")
+public class TripAttraction {
+    @ApiModelProperty(value = "여행 계획 아이디")
+    private int planId;
+    @ApiModelProperty(value = "여행지 아이디")
+    private int contentId;
+    @ApiModelProperty(value = "여행지 방문일")
+    private String visitDate;
+}
