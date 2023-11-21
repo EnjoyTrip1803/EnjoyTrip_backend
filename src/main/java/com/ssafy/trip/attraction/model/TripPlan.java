22d0d81005da6ed98072e5fccb9ad201929a7ccb
서성원 <zsa332@gmail.com>
2023-11-21 14:21:28 +0900

    refactor: title 추가

--- src/main/java/com/ssafy/trip/attraction/model/TripPlan.java
+++ src/main/java/com/ssafy/trip/attraction/model/TripPlan.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Data
 @ApiModel(value = "TripPlan (여행 계획 정보)")
 public class TripPlan {
+    @ApiModelProperty(value = "계획 제목")
+    private String title;
     @ApiModelProperty(value = "계획 아이디")
     private int planId;
     @ApiModelProperty(value = "생성자 아이디")
