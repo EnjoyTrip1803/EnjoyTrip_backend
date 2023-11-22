9b30c2dc637946850c98ecbd8b0fbc92609bd595
서성원 <zsa332@gmail.com>
2023-11-22 17:48:22 +0900

    Feat: 시도 데이터에 img 추가

--- src/main/java/com/ssafy/trip/attraction/model/AreaCode.java
+++ src/main/java/com/ssafy/trip/attraction/model/AreaCode.java
@@ -6,9 +6,9 @@ import lombok.Data;
 @Data
 @ApiModel(value = "AreaCode (시도, 구군 정보)", description = "시도, 구군에 대한 코드와 이름을 가진 Class")
 public class AreaCode {
-
 	private int sidoCode;
 	private String sidoName;
+	private String sidoImg;
 	private int gugunCode;
 	private String gugunName;
 }
