0d6028e9cb59cce7e3624c5e57fabf6411601239
seheon <quwe153@naver.com>
2023-12-19 18:01:29 +0900

    테스트

--- src/main/java/com/ssafy/trip/socket/SocketController.java
+++ src/main/java/com/ssafy/trip/socket/SocketController.java
@@ -3,6 +3,7 @@ package com.ssafy.trip.socket;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
@@ -10,11 +11,13 @@ import java.time.LocalDateTime;
 @Controller
 @RequiredArgsConstructor
 public class SocketController {
+
     private final SimpMessageSendingOperations simpMessageSendingOperations;
 
     @MessageMapping("/chat")
-    public void socketChat(Chat chat) {
+    public void socketChat(Chat chat, StompHeaderAccessor session) {
         chat.setCreateAt(LocalDateTime.now());
+        System.out.println(session.getSessionAttributes().get("userId"));
         simpMessageSendingOperations.convertAndSend("/topic/channel/"+ chat.getPlanId(), chat);
     }
 
@@ -23,7 +26,9 @@ public class SocketController {
         simpMessageSendingOperations.convertAndSend("/topic/channel/"+ plan.getPlanId(), plan);
     }
 
-
-
+    @MessageMapping("/enter")
+    public void socketPlan(Plan plan){
+        simpMessageSendingOperations.convertAndSend("/topic/channel/"+ plan.getPlanId(), plan);
+    }
 
 }
