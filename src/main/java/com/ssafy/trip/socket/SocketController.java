ef709e4e3bef739760c9ab292f4367c8688bd66d
seheon <quwe153@naver.com>
2023-12-20 18:16:56 +0900

    feat: 사용자 유무 로직 구현

--- src/main/java/com/ssafy/trip/socket/SocketController.java
+++ src/main/java/com/ssafy/trip/socket/SocketController.java
@@ -7,17 +7,17 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 @Controller
 @RequiredArgsConstructor
 public class SocketController {
-
+    private Map<Integer, Set<String>> rooms = new HashMap<>();
     private final SimpMessageSendingOperations simpMessageSendingOperations;
 
     @MessageMapping("/chat")
-    public void socketChat(Chat chat, StompHeaderAccessor session) {
+    public void socketChat(Chat chat) {
         chat.setCreateAt(LocalDateTime.now());
-        System.out.println(session.getSessionAttributes().get("userId"));
         simpMessageSendingOperations.convertAndSend("/topic/channel/"+ chat.getPlanId(), chat);
     }
 
@@ -27,8 +27,40 @@ public class SocketController {
     }
 
     @MessageMapping("/enter")
-    public void socketPlan(Plan plan){
-        simpMessageSendingOperations.convertAndSend("/topic/channel/"+ plan.getPlanId(), plan);
+    public void socketEnter(Room room) {
+        Integer planId = room.getPlanId();
+        Integer userId = room.getUserId();
+
+        // 방이 없을때
+        if (!rooms.containsKey(planId)){
+            rooms.put(planId, new HashSet<>());
+        }
+
+        String nickname = null; // userId로 닉네임 찾기
+        rooms.get(planId).add(nickname);
+
+        Member member = new Member();
+        member.setType("room");
+        member.setMembers(rooms.get(planId));
+        simpMessageSendingOperations.convertAndSend("/topic/channel/"+ planId, member);
     }
+
+    @MessageMapping("/exit")
+    public void socketExit(Room room) {
+        Integer planId = room.getPlanId();
+        Integer userId = room.getUserId();
+        rooms.get(planId).remove(userId);
+
+        String nickname = null; // userId로 닉네임 찾기
+        rooms.get(planId).add(nickname);
+
+        Member member = new Member();
+        member.setType("room");
+        member.setMembers(rooms.get(room.getPlanId()));
+        simpMessageSendingOperations.convertAndSend("/topic/channel/"+ planId, member);
+    }
+
+
+
 
 }
